ab0e970609c9859f6b3a164916a0946f14843f35
Thijs <thijsvb@gmail.com>
2016-10-04 12:28:31 +0200

    Update sketch.js

--- Thingies/disks/sketch.js
+++ Thingies/disks/sketch.js
@@ -14,19 +14,7 @@ function setup() {
 
 function doStuff() {
   disks[index] = new Disk(random(width), random(height));
-  console.log(disks);
-  var grow = true;
-  while (grow) {
-    ++disks[index].dia;
-
-    for (var i = 0; i != index; ++i) {
-      if (disks[index].olap(disks[i])) {
-        grow = false;
-        break;
-      }
-    }
-  }
-  --disk[index].dia;
+  
   fill(disks[index].col);
   ellipse(disks[index].x, disks[index].y, disks[index].dia, disks[index].dia);
   ++index;
@@ -38,15 +26,11 @@ function Disk(x, y) {
   this.y = y;
   this.min = 10;
   this.max = 100;
-  this.dia = this.min;
+  this.rad = this.min
+  this.dia = function(){return this.rad*2;};
   this.col = color(random(255), 255, 255);
   this.olap = function(other) {
-    console.log(this.dia);
-
-    if (this.dia > width) {
-      stoppit
-    }
-    if (dist(this.x, this.y, other.x, other.y) < (this.dia / 2 + other.dia / 2) || this.dia >= 100) {
+    if (dist(this.x, this.y, other.x, other.y) < (this.rad + other.rad) || this.dia >= this.max) {
       return true;
     } else {
       return false;
