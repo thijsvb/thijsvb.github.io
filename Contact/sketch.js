cba303c38cf1f5478a75e7d9764338d3e32296d3
Thijs <thijsvb@gmail.com>
2016-11-17 12:00:22 +0100

    Update sketch.js

--- Contact/sketch.js
+++ Contact/sketch.js
@@ -1,3 +1,21 @@
+var bf, code, email;
+
 function setup() {
-  noCanvas();
+  var can = canvas(800, 400);
+  var middle = select("#middle");
+  can.child(middle);
+  bf = new BFJSInterpreter();
+  loadStrings("email.txt", gotCode);
+}
+
+function gotCode(txt) {
+  code = txt[0];
+  
+  bf.execute(code);
+  email = bf.outputStream;
+  
+  background(0);
+  fill(255);
+  textAlign(CENTER, CENTER);
+  text(email, width/2, height/2);
 }
