37a879ac877f3c0d36c716dd5be8f506f43388bb
Thijs <thijsvb@gmail.com>
2016-11-17 11:34:58 +0100

    Update sketch.js

--- Contact/sketch.js
+++ Contact/sketch.js
@@ -1,10 +1,3 @@
 function setup() {
   noCanvas();
-  brainfuck = new Brainfuck(new BrainfuckMemoryInfinite());
-  brainfuck.code.source('+++.[-].');
-  brainfuck.run(done);
-}
-
-function done() {
-  console.log("done!");
 }
