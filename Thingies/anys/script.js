c00af351ed0d8489a38ab98248a5b4d0ab886b68
stefano giovinazzo <emmagiovinazzo@RN-145-97-142-86.eduroam.rug.nl>
2018-05-10 14:42:18 +0200

    Remove .DS_Store

---
+++ Thingies/anys/script.js
@@ -0,0 +1,18 @@
+let from, to
+
+function setup() {
+  from = createSelect();
+  from.option('W');
+  from.option('T');
+  from.option('L');
+  from.option('E');
+  to = createSelect();
+  to.option('W');
+  to.option('T');
+  to.option('L');
+  to.option('E');
+}
+
+function loop() {
+
+}
