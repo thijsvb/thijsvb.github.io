f0b7da6ecb56bd4c012e1c7ef2205b6ddea542f4
stefano giovinazzo <emmagiovinazzo@RN-145-97-142-86.eduroam.rug.nl>
2018-05-10 15:09:55 +0200

    add buttons

--- Thingies/anys/script.js
+++ Thingies/anys/script.js
@@ -1,4 +1,5 @@
-let from, to
+let from, to;
+let more, less;
 
 function setup() {
   from = createSelect();
@@ -6,13 +7,34 @@ function setup() {
   from.option('T');
   from.option('L');
   from.option('E');
+
+  f = select("#from");
+  from.parent(f);
+
   to = createSelect();
   to.option('W');
   to.option('T');
   to.option('L');
   to.option('E');
+
+  t = select("#to");
+  to.parent(t);
+
+  more = select("#more");
+  less = select("#less");
+
+  more.mouseClicked(addAny);
+  less.mouseClicked(useAny);
 }
 
-function loop() {
+function addAny() {
+  giver = from.value();
+  taker = to.value();
+  console.log(giver+taker);
+}
 
+function useAny() {
+  giver = from.value();
+  taker = to.value();
+  console.log(giver+taker);
 }
